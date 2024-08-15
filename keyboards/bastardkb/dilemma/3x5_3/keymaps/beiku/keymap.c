af83c435e5aae6d2edcb72dc475010e06a366ed4
beiku <pbeiku@gmail.com>
2024-08-15 13:45:13 +0900

    beiku

--- keyboards/bastardkb/dilemma/3x5_3/keymaps/beiku/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3/keymaps/beiku/keymap.c
@@ -56,10 +56,10 @@ enum combos {
 /** \brief QWERTY layout (3 rows, 10 columns). */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT_split_3x5_3(
-       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-       LCTL_T(KC_A), LSFT_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), KC_G, KC_H, RGUI_T(KC_J), RALT_T(KC_K), RSFT_T(KC_L), RCTL_T(KC_SCLN),
-       PT_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, PT_SLSH,
-                      KC_TAB, SPC_NUM, ESC_FUN, ENT_SYM, BSP_NAV, CAP_MED
+               KC_Q,         KC_W,         KC_E,         KC_R,    KC_T,    KC_Y,         KC_U,         KC_I,         KC_O,            KC_P,
+       LCTL_T(KC_A), LSFT_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F),    KC_G,    KC_H, RGUI_T(KC_J), RALT_T(KC_K), RSFT_T(KC_L), RCTL_T(KC_SCLN),
+               PT_Z,         KC_X,         KC_C,         KC_V,    KC_B,    KC_N,         KC_M,      KC_COMM,       KC_DOT,         PT_SLSH,
+                                         KC_TAB,      SPC_NUM, ESC_FUN, ENT_SYM,      BSP_NAV,      CAP_MED
   ),
 
 /*
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   KC_F7,   KC_F8,   KC_F9,  KC_F12,
     KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI, XXXXXXX, XXXXXXX,   KC_F4,   KC_F5,   KC_F6,  KC_F11,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   KC_F1,   KC_F2,   KC_F3,  KC_F10,
-                      XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX
+                      XXXXXXX, XXXXXXX, _______, XXXXXXX,  KC_DEL, XXXXXXX
   ),
 
 /**
