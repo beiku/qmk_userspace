9c6ca233324e972804f66b5f95b87006b4f9b352
beiku <pbeiku@gmail.com>
2025-07-26 13:09:20 +0900

    beiku delete combos

--- keyboards/bastardkb/dilemma/3x5_3/keymaps/beiku/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3/keymaps/beiku/keymap.c
@@ -176,18 +176,18 @@ combo_t key_combos[] = {
   // [LT_SFT] = COMBO(lt_combo, S(KC_T)),
 };
 
-// bool get_combo_must_press_in_order(uint16_t combo_index, combo_t *combo) {
-//     switch (combo_index) {
-//         case LQ_SFT:
-//         case LW_SFT:
-//         case LE_SFT:
-//         case LR_SFT:
-//         case LT_SFT:
-//             return true;
-//         default:
-//             return false;
-//     }
-// }
+bool get_combo_must_press_in_order(uint16_t combo_index, combo_t *combo) {
+    switch (combo_index) {
+        // case LQ_SFT:
+        // case LW_SFT:
+        // case LE_SFT:
+        // case LR_SFT:
+        // case LT_SFT:
+            return true;
+        default:
+            return false;
+    }
+}
 
 #ifdef POINTING_DEVICE_ENABLE
 #    ifdef DILEMMA_AUTO_SNIPING_ON_LAYER
